b4f81d287a0262a14b6a8b2e1a9184853f0c9b45
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxGridView;
+
+namespace WebApplication11 {
+    public partial class _Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+        }
+
+        protected void ASPxGridView1_CustomSummaryCalculate(object sender, DevExpress.Data.CustomSummaryEventArgs e) {
+            ASPxSummaryItem incomeSummary = (sender as ASPxGridView).TotalSummary["Income"];
+            ASPxSummaryItem expenseSummary = (sender as ASPxGridView).TotalSummary["Expense"];
+            Decimal income = Convert.ToDecimal(((ASPxGridView)sender).GetTotalSummaryValue(incomeSummary));
+            Decimal expense = Convert.ToDecimal(((ASPxGridView)sender).GetTotalSummaryValue(expenseSummary));
+            e.TotalValue = income - expense;
+        }
+        
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to calculate the total summary value based on values of other summaries
+
+
+<p>This sample illustrates how to calculate the total summary value based on values of other summaries</p>
+
+<br/>
+
+
